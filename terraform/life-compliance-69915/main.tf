8cece0fe84ddf5d4caf37cf57911cb7bd09bb933
globalworming <andreas.worm@headissue.com>
2022-11-25 23:15:27 +0100

    add missing permissions

--- terraform/life-compliance-69915/main.tf
+++ terraform/life-compliance-69915/main.tf
@@ -8,6 +8,17 @@ module "enable_apis" {
   source = "./module/enable_apis"
 }
 
+data "google_project" "project" {}
+
+locals {
+  cloud_build_service_account = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "cloud_run_admin" {
+  project = var.project_id
+  role    = "roles/run.developer"
+  member  = local.cloud_build_service_account
+}
 
 module "single_page_application" {
   source = "./service/single_page_application"
