6dbbc7b9d00814afeeb0c069804da0d5f714e7a8
globalworming <andreas.worm@headissue.com>
2022-11-28 09:18:42 +0100

    add nginx conf

--- terraform/life-compliance-69915/main.tf
+++ terraform/life-compliance-69915/main.tf
@@ -10,8 +10,10 @@ module "enable_apis" {
 
 data "google_project" "project" {}
 
+### START permissions needed to deploy from cloudbuild run
 locals {
   cloud_build_service_account = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+  compute_service_account_id  = "projects/${var.project_id}/serviceAccounts/${data.google_project.project.number}-compute@developer.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cloud_run_admin" {
@@ -19,6 +21,15 @@ resource "google_project_iam_member" "cloud_run_admin" {
   role    = "roles/run.developer"
   member  = local.cloud_build_service_account
 }
+
+
+resource "google_service_account_iam_member" "default-compute-account" {
+  service_account_id = local.compute_service_account_id
+  role               = "roles/iam.serviceAccountUser"
+  member             = local.cloud_build_service_account
+}
+### END permissions needed to deploy from cloudbuild run
+
 
 module "single_page_application" {
   source = "./service/single_page_application"
