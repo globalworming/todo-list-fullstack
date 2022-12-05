ce377913494bfeffe295a9ec77594a8c1bfe4a40
globalworming <andreas.worm@headissue.com>
2022-12-05 22:00:34 +0100

    log datastore connection error

--- todo-service/src/main/java/com/headissue/compliance/HealthService.java
+++ todo-service/src/main/java/com/headissue/compliance/HealthService.java
@@ -1,10 +1,15 @@
 package com.headissue.compliance;
 
 import io.grpc.health.v1.HealthCheckResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class HealthService {
+
+    private final Logger logger = LoggerFactory.getLogger(HealthService.class);
+
     private DataStoreClient dataStoreClient;
 
     public HealthService(DataStoreClient dataStoreClient) {
@@ -16,6 +21,7 @@ public class HealthService {
             var key = dataStoreClient.createRandomDocument();
             dataStoreClient.deleteDocument(key);
         } catch (Exception e) {
+            logger.error("datastore", e);
             return Map.of("dataStore", HealthCheckResponse.ServingStatus.NOT_SERVING);
         }
         return Map.of("dataStore", HealthCheckResponse.ServingStatus.SERVING);
