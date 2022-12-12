8dee25cdce22c7f55d8ced9b19c88f36a67d7254
globalworming <andreas.worm@headissue.com>
2022-12-12 13:22:09 +0100

    add cypress component testing

---
+++ single-page-application/cypress.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack',
+    },
+  },
+});
---
+++ single-page-application/src/e2e/app.test.js
@@ -0,0 +1,17 @@
+import assert from 'assert';
+import request from 'supertest';
+
+describe('checking backend health', () => {
+  it('responds with ok', (done) => {
+    request('https://bff-fg5blhx72q-ey.a.run.app')
+      .get('/health')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        assert(response.body.services.find((it) => it.name === 'bff').serving, 'service bff ok');
+        assert(response.body.services.find((it) => it.name === 'todo').serving, 'service todo ok');
+        done();
+      });
+  });
+});
