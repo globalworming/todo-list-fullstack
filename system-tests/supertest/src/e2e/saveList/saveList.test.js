4072cf6980fc29ff42a37e6ea448a1513fc8c8ff
Umer <umer.farooq@williamhill.co.uk>
2022-12-19 20:19:21 +0000

    add save list api test

---
+++ system-tests/supertest/src/e2e/saveList/saveList.test.js
@@ -0,0 +1,55 @@
+import assert from 'assert';
+import request from 'supertest';
+
+describe('saving a todo list', () => {
+  it('responds with "Bad Request" when list is empty', (done) => {
+    request('https://bff-fg5blhx72q-ey.a.run.app')
+      .post('/toDoLists')
+      .send({
+        name: 'some list',
+        todoItem: [],
+      })
+      .set('Accept', 'application/json')
+      //.expect('Content-Type', /json/)
+      .expect(400)
+      .then((response) => {
+        assert(response.body.error === "EMPTY_LIST",'list is empty error');
+        //assert(response.body.services.find((it) => it.name === 'todo').serving, 'service todo ok');
+        done();
+      });
+  });
+
+
+  it('responds ok when list is created successfully', (done) => {
+      request('https://bff-fg5blhx72q-ey.a.run.app')
+        .post('/toDoLists')
+        .send({
+                name: 'some list',
+                todoItem: [
+                {
+
+                 description: 'feed the cat',
+
+                }
+
+                ],
+              })
+        .set('Accept', 'application/json')
+        //.expect('Content-Type', /json/)
+        .expect(200)
+        .end(done);
+    });
+});
+
+
+
+//{
+//   "message":"error",
+//   "code":1,
+//   "type": "EMPTY_LIST_NOT_ALLOWED",
+//   "fields" : [
+//
+//        "item": "error_empty";
+//
+//   ],
+//}
