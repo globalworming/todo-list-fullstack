311cfb8e346f2b8b278152da34baf392b2629b25
globalworming <andreas.worm@headissue.com>
2023-01-09 10:08:48 +0100

    fix content type, docker compose network, mock datastore

--- system-tests/supertest/src/e2e/saveList/saveList.test.js
+++ system-tests/supertest/src/e2e/saveList/saveList.test.js
@@ -3,7 +3,7 @@ import request from 'supertest';
 
 describe('saving a todo list', () => {
   it('responds with "Bad Request" when list is empty', (done) => {
-    request('https://bff-fg5blhx72q-ey.a.run.app')
+    request('localhost:8080')
       .post('/toDoLists')
       .send({
         name: 'some list',
@@ -12,50 +12,48 @@ describe('saving a todo list', () => {
       .set('Accept', 'application/json')
       .expect(400)
       .then((response) => {
-        assert(response.body.error === "EMPTY_LIST",'list is empty error');
-        //assert(response.body.services.find((it) => it.name === 'todo').serving, 'service todo ok');
+        assert(response.body.error === 'EMPTY_LIST', 'list is empty error');
+        // assert(response.body.services.find((it) => it.name === 'todo').serving, 'service todo ok');
         done();
-    });
+      });
   });
 
-    it('responds ok when list is created successfully', (done) => {
-        request('https://bff-fg5blhx72q-ey.a.run.app')
-            .post('/toDoLists')
-            .send({
-                name: 'some list',
-                todoItem: [
-                    {
-                        description: 'feed the cat',
-                    },
-                ],
-            })
-            .set('Accept', 'application/json')
-            //.expect('Content-Type', /json/)
-            .expect(200)
-        done();
-    });
-});
-
-it('responds not ok when list already exists', (done) => {
+  it('responds ok when list is created successfully', (done) => {
     request('https://bff-fg5blhx72q-ey.a.run.app')
-        .post('/toDoLists')
-        .send({
-            name: 'name already',
-            todoItem: [],
-        })
-        .set('Accept', 'application/json')
-        //.expect('Content-Type', /json/)
-        .expect(400)
-        .then((response) => {
-            assert(response.body.error === 'NAME_LIST_ALREADY_TAKEN','see message name already taken');
-            //assert(response.body.services.find((it) => it.name === 'todo').serving, 'service todo ok');
-        .end(done);
+      .post('/toDoLists')
+      .send({
+        name: 'some list',
+        todoItem: [
+          {
+            description: 'feed the cat',
+          },
+        ],
+      })
+      .set('Accept', 'application/json')
+    // .expect('Content-Type', /json/)
+      .expect(200);
+    done();
+  });
 
-        });
+  it('responds not ok when list already exists', (done) => {
+    request('https://bff-fg5blhx72q-ey.a.run.app')
+      .post('/toDoLists')
+      .send({
+        name: 'name already',
+        todoItem: [],
+      })
+      .set('Accept', 'application/json')
+    // .expect('Content-Type', /json/)
+      .expect(400)
+      .then((response) => {
+        assert(response.body.error === 'NAME_LIST_ALREADY_TAKEN', 'see message name already taken');
+        // assert(response.body.services.find((it) => it.name === 'todo').serving, 'service todo ok');
+        done();
+      });
+  });
 });
 
-
-//{
+// {
 //   "message":"error",
 //   "code":1,
 //   "type": "EMPTY_LIST_NOT_ALLOWED",
@@ -64,4 +62,4 @@ it('responds not ok when list already exists', (done) => {
 //        "item": "error_empty";
 //
 //   ],
-//}
+// }
