131ac6f93d25f8bf51b3908536c8cdf18afa5e15
globalworming <andreas.worm@headissue.com>
2023-02-16 13:45:54 +0100

    lint

--- single-page-application/cypress.config.js
+++ single-page-application/cypress.config.js
@@ -1,17 +1,15 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   component: {
     devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+      framework: 'create-react-app',
+      bundler: 'webpack',
     },
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+
   },
 });
