9c04093ebe05eea1161401c0b6c069ec1fd94bed
globalworming <andreas.worm@headissue.com>
2023-02-20 14:57:13 +0100

    refactor

--- single-page-application/src/components/TodoList.test.jsx
+++ single-page-application/src/components/TodoList.test.jsx
@@ -36,15 +36,20 @@ test('where we change the list name', async () => {
   ensureListNameIs('my list');
 });
 
-test('where we add todo items', async () => {
-  render(todoList);
+async function addToDos(...items) {
   const input = screen.getByPlaceholderText('What needs to be done?');
   expect(input).toBeInTheDocument();
   await user.click(input);
   await act(async () => {
-    await user.keyboard('feed cat{Enter}');
-    await user.keyboard('feed dog{Enter}');
+    for (const item of items) {
+      await user.keyboard(`${item}{Enter}`);
+    }
   });
+}
+
+test('where we add todo items', async () => {
+  render(todoList);
+  await addToDos('feed cat', 'feed dog');
   expect(screen.getByTestId('count')).toHaveTextContent('2 items left');
 });
 
@@ -53,13 +58,7 @@ test('where we save a list', async () => {
     rest.post(`${process.env.REACT_APP_GATEWAY}/toDoLists`, (req, res, ctx) => res(ctx.status(200))),
   );
   render(todoList);
-  const input = screen.getByPlaceholderText('What needs to be done?');
-  expect(input).toBeInTheDocument();
-  await user.click(input);
-  await act(async () => {
-    await user.keyboard('feed cat{Enter}');
-    await user.keyboard('feed dog{Enter}');
-  });
+  await addToDos('feed cat', 'feed dog');
   await user.click(screen.getByTestId('save-list'));
   await waitFor(() => {
     expect(screen.getByTestId('saved-list')).toBeInTheDocument();
